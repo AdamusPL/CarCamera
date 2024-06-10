993474242ef83843d840b4a0c933568927ef3a45
AdamusPL <adam.czekalski.szczecin@gmail.com>
2024-06-10 23:00:29 +0200

    fix

--- newMain.py
+++
@@ -1,6 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="oMjehIs6m5IK1UqNzdnR")
-project = rf.workspace("trylogy").project("ripo-vopt8")
-version = project.version(3)
-dataset = version.download("yolov8")
-
